8d1d4f19ee7c21f9ca2ce772cf1add8592c5af46
ozlemIstek <ozlem.istek@gmail.com>
2019-01-19 18:49:25 +0300

    ikinci ilk commit

--- src/main/java/com/example/demo2/Demo2Application.java
+++ src/main/java/com/example/demo2/Demo2Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-
+//eneme özlem
 public class Demo2Application {
 
     public static void main(String[] args) {
