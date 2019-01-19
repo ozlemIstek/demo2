c45c5b003e7cb1166c9b68f87a9a6b29b83a929d
ozlemIstek <ozlem.istek@gmail.com>
2019-01-19 20:37:25 +0300

    my comment

--- src/main/java/com/example/demo2/Demo2Application.java
+++ src/main/java/com/example/demo2/Demo2Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-//eneme özlem
+//dghjsghhgdfghdsfhhj
 public class Demo2Application {
 
     public static void main(String[] args) {
